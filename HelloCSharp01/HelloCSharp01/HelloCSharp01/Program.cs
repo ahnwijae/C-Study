35c8049a77d2387a36d769efb8a5a622f57eb32b
ahnwijae <wijae1612@naver.com>
2024-03-19 10:14:46 +0900

    VS commit

---
+++ HelloCSharp01/HelloCSharp01/HelloCSharp01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WirteLine = System.out.println
+            //Console.Write = System.out.print
+            // cw tab*2
+            Console.WriteLine("안녕");
+            Console.WriteLine( "Hello");
+            Console.Write("World!");// ctrl + d = 한줄복붙
+            Console.Write("World!");
+            Console.WriteLine("World!");
+            Console.WriteLine( ); // 그냥 한 줄 띄우기
+            //cw는 코드조각이며 사용자가 새로운 코드조각 생성가능
+            Console.Write( "write");
+            Console.WriteLine("writeLine");
+            Console.WriteLine("writeLine");
+            Console.WriteLine("writeLine");
+            Console.Write( "write");
+            Console.Write( "write");
+        }
+    }
+}
